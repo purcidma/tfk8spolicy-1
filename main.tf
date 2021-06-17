cc9fb86da3548b14a7d3b76c770f1fa3d322c7d6
EnigmaCode <72094665+purcidma@users.noreply.github.com>
2021-06-17 13:30:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "global" {
   backend = "remote"
   config = {
-    organization = "CiscoDevNet"
+    organization = "Lab14"
     workspaces = {
       name = var.globalwsname
     }
@@ -12,15 +12,6 @@ data "terraform_remote_state" "global" {
 variable "globalwsname" {
   type        = string
   description = "TFCB workspace name that has all of the global variables"
-}
-variable "api_key" {
-  type        = string
-  description = "API key for Intersight user"
-}
-
-variable "secretkey" {
-  type        = string
-  description = "Secret key for Intersight user"
 }
 
 terraform {
@@ -33,8 +24,35 @@ terraform {
 }
 
 provider "intersight" {
-  apikey    = var.api_key
-  secretkey = var.secretkey
+  apikey    = "5981e8973e95200001018a24/59a844f6f11aa10001d90cdb/60cb8f3e7564612d302f8dfa"
+  secretkey = "-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAnGZUZXUWQ2SlElltOZ1CI7SyINvZJg2BoZEhbicy41Xy6ymT
+Oz6NUZnWWf0Rsrot6lx/isLLxaq5wJtbpsKua3tQo9mpAlyX2Lc7j8u/uu88EywP
+rh22bbEY59zZiXa5gkC5FNOmJPAOFwZ1xdLTd+QQGD5ZHt+03rOV57iFR3c9OOel
+NQ13rtRSu+sDs0eCReFzJDly/WUmo0hi0vwLc2+7s31h4KPYAGJv9yAPl3QflDEU
+y049EC4t6vJ5+xG4BVqIaMf682Hkfu3paH1xfciVD9P3fWpz4sBrp5PVMpyXCDlZ
+35lWaZyRaZkH/D78YAI9Nc2wisOuZfOMiEP1jwIDAQABAoIBAC9x135js5pEKNNV
+L44/x3Wfdj4Dk284v69soHCTEZvnUebG2PQq+KHdgioQXFMUqaMj5kkI6NoQ/YKR
+Xo9LFbBjW0YpXFzsC+BhTX9EtugVdOr3/dW7C8Fg7QZBd/h8fLAnxD2uKwOfkxan
+Dgkx+JaxJ7TDDTQRdwPsBLijwEK6Tvi/kFpTF36TVsLKUfO0+/mNOD+8oFmgzlKg
+4P70Xt7spj/CaZUMpIEJhObKP3bk7anuXiY+u1zPkpLsoyKG5QZz6fCI7b3oavxP
+PY+D82kmZwNCCe+iq34nfol4odW9m4y5i08SER9pX2jnvam4EoryO3Di46Djuzx6
+AsWcwrkCgYEAzIzXoVCbOEiudqlX1XKrsJ61TcsIBu/WDpOGuLQFpOlG5X7/zhRC
+zSM4CP88zitduE9BJsdyoJudYpoNmwGJP7IfZDO0zzZWOZKG1C+g5V20zz/5uzdn
+1sUOUIxblPDSWq6pJ8g3e6/NsMzeutsbxLVqkWAeYJ/KdNr9nIWj8U0CgYEAw70K
+OCxA4YuvdHCcn1+Da/+EMuHd97WfXuPlajQdMzWupfbG+4bD45t0uhHsHIQY64bd
+drFEzibu7iDCLj224hOH++keD5efw1QBSDQq94chllPMapT80U6j0RrYGO1QTabY
+9gOWBygngmqLrj0sYdmVBsspDc/qRf3oNTbLVEsCgYEAmu4muQmbt+LdOgWEAKkN
+QAbF3nlkyBRKttmo1ieMit8iEtvBl26jNktxuubQQHx6TQrCl0PEH8AeUjvLCFf+
+g98/hZ7gWX6Xip3gP8EfhfsW409asSIDJZo2AG5/Q22wdn0KpJYy6B09l6dlIHSr
+MLAUWq5J8/ez2hSwuShEB5UCgYA9+ILcx/3+qqfxGJotxyKnta0YIvSQsXr0oviG
+SFuaU/uoZdoX1lH8pMIvCu+TE6uEHh3Nr1AWaLMqx0pTM4zMRNy/v82ZqCqXv+fs
+AUA9QBY9LujAMc1dQyWQVYxnT/MlspZsRTRhra/clXkFwC5mCGorTXUA/3uvjzTO
+bDuxwwKBgGSBjMXFCdC6ky2BsqQhxZEJXpfnKz8QxbG8IHZ91LiNzQAIyVP9TsKl
+cKv+JQNqYe18+fUCowhCKtdaZ3/blflmz3qm8xaq2ZhG4i5epupDvGINBKZpS9vY
+jj9B8j6M/u186dQAcmxB16lNiS4kdju6WL9SuQ1GMUF265/fLe0y
+-----END RSA PRIVATE KEY-----
+"
   endpoint = "https://intersight.com"
 }
 
